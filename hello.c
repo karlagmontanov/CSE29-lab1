1c4722800675eccea98ee4f2f497107d1609fb36
karlagmontanov <166085411+karlagmontanov@users.noreply.github.com>
2024-10-01 22:22:39 +0000

    commit

--- hello.c
+++ hello.c
@@ -3,9 +3,11 @@
 int main() {
   char hello[] = "Hello!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello3 [] = "Hello there!";
 
   puts(hello);
   puts(hello2);
+  puts(hello3);
 
   return 0;
 }
